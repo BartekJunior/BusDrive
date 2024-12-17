c6d40ec997757ca513c1b7f22bc61e8a8d90f497
BartekJunior <bartekprzybysz@gmail.com>
2024-12-17 12:38:04 +0100

    php.send DONE with validate.js, DYNAMIC FORM DONE

--- send.php
+++ send.php
@@ -7,16 +7,17 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $email = htmlspecialchars($_POST['email']);
     $phone = htmlspecialchars($_POST['phone']);
     $message = htmlspecialchars($_POST['message']);
+    $subject = htmlspecialchars($_POST['subject']); // Pobieramy temat wiadomości
 
     // Ustawienia wiadomości
     $to = "kontakt@busdrive.pl"; // Twój adres e-mail
-    $subject = "Nowa wiadomość z formularza kontaktowego";
     $headers = "From: $email\r\n";
     $headers .= "Reply-To: $email\r\n";
     $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
     // Treść e-maila
     $body = "Otrzymałeś nową wiadomość:\n\n";
+    $body .= "Temat: $subject\n"; // Dodajemy temat do treści e-maila
     $body .= "Imię: $name\n";
     $body .= "E-mail: $email\n";
     $body .= "Telefon: $phone\n";
